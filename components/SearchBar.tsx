acc34ea3ecbfc7d06cc22050b17f0559c8706314
Thới Duy Phát <88571858+duyphatbk@users.noreply.github.com>
2024-02-29 18:06:15 +0700

    search bar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { SearchManuFacturer } from ".";
+import { useState } from "react";
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState("");
+    const handleSearch = () => {}
+    return (
+        <form action="" className="searchbar" onSubmit={handleSearch}>
+            <div className="searchbar__item">
+                <SearchManuFacturer 
+                    manufacturer = {manufacturer}
+                    setManufacturer = {setManufacturer}
+                />
+            </div>
+        </form>
+    )
+}
+
+export default SearchBar
